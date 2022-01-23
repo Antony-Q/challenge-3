210992ff02318458dad21a233d8d7a5a74f193c9
Antony <antonyq1992@gmail.com>
2022-01-23 11:49:55 -0600

    created option comfirms

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,8 @@
 // Assignment code here
-
+var upperCase = ["A", "B", "C"]
+var lowerCase = ["a", "b", "c"]
+var number = ["1", "2", "3"]
+var specChar = ["!", "@", "#"]
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
@@ -16,11 +19,50 @@ generateBtn.addEventListener("click", writePassword);
 
 function generatePassword() {
   var passLength = parseInt(window.prompt("How many characters in your password? Pick from 1 to 20."));
-  if (passLength < 20 || passLength > 1) {
+  if (passLength > 20 || passLength < 1) {
     return null;
   }
-  var upperCase = window.confirm('Do you want your password to include uppercase characters? Click "ok" if yes.');
-  var lowerCase = window.confirm('Do you want your password to include lower case characters? Click "ok" if yes.');
+
+  var passUpper = window.confirm('Do you want your password to include uppercase characters? Click "ok" if yes.');
+  var passLower = window.confirm('Do you want your password to include lower case characters? Click "ok" if yes.');
   var passNum = window.confirm('Do you want your password to include numbers? Click "ok" if yes.');
   var passSpec = window.confirm('Do you want your password to be include special characters? Click "ok" if yes.');
+  var possibleChar = [];
+
+  var generatedPass = [];
+
+  if (passUpper) {
+    possibleChar = possibleChar.concat(upperCase);
+  }
+  if (passUpper) {
+    possibleChar = possibleChar.concat(lowerCase);
+  }
+  if (passUpper) {
+    possibleChar = possibleChar.concat(number);
+  }
+  if (passUpper) {
+    possibleChar = possibleChar.concat(specChar);
+  }
+  for (var i = 0; i < passLength; i++) {
+    var randomLetter = possibleChar[Math.floor(Math.
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      random() * (possibleChar.length))]
+    generatedPass.push(randomLetter);
+  }
+  return generatedPass;
 };
